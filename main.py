b6087c52f3d6f0509eb4c8da302618287d65e24b
willis3333 <60554260+willis3333@users.noreply.github.com>
2021-06-18 17:06:42 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+# Brand Name generator using inputs from user
+print("welcome to the Automatic Brand Name Generator")
+name = input("what is your Last name? ")
+street = input("what is your street name? ")
+print("Your brand name could be: ", street[0:2] + name[0:3],"\n")
---
+++ readme.txt
@@ -0,0 +1,3 @@
+This program utilizes inputs, variable, concotenation, and string manipulation to create a Brand Name using user input
+
+To use, run code and enter your data and it will generate your Brand Name
